c1738e1f38611fc054acea5e629a224c2882943c
Moritz Spengler <moco_spengler@gmx.de>
2025-11-30 11:38:25 +0100

    add comment to test files

--- jarvis/src/main/ME/pages/MeLandingPage.test.tsx
+++ jarvis/src/main/ME/pages/MeLandingPage.test.tsx
@@ -1,1 +1,2 @@
 // This is a test file for the MeLandingPage component
+// We will not add test at the moment while creating the MVP
--- jarvis/src/main/WE/pages/WeLandingPage.test.tsx
+++ jarvis/src/main/WE/pages/WeLandingPage.test.tsx
@@ -1,1 +1,2 @@
 // This is a test file for the WeLandingPage component
+// We will not add test at the moment while creating the MVP
